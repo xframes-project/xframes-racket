d23996be3f1763e0856cff00890002fa97c271a4
Andrea Mancuso <and.mancuso@googlemail.com>
2025-01-02 17:32:56 +0100

    Update font-defs.json, main.rkt, and 2 more files...

---
+++ main.rkt
@@ -0,0 +1,94 @@
+#lang racket
+(require ffi/unsafe
+         ffi/unsafe/define
+         ffi/unsafe/atomic
+         ffi/unsafe/os-async-channel
+         ffi/unsafe/nsstring
+         ffi/unsafe/alloc
+         ffi/unsafe/custodian
+         ffi/unsafe/schedule
+         ffi/unsafe/os-thread
+         racket/match
+         racket/runtime-path
+         json)
+
+
+(define xframes
+  (match (system-type)
+    ['unix (ffi-lib "./libxframesshared.so")]
+    ['windows (ffi-lib "./xframesshared.dll")]))
+
+(define _OnInitCb (_fun #:async-apply (lambda (f) (f)) -> _void))
+(define _OnTextChangedCb (_fun #:async-apply (lambda (f) (f)) _int _string -> _void))
+
+(define init
+  (get-ffi-obj "init" xframes (
+    _fun 
+        _string 
+        _string 
+        _string
+        _OnInitCb
+        _OnTextChangedCb
+        (_fun _int _int -> _void)
+        (_fun _int _float -> _void)
+        (_fun _int _bool -> _void)
+        (_fun _int _pointer _int -> _void)
+        (_fun _int -> _void)
+         -> _void)))
+
+(define setElement
+  (get-ffi-obj "setElement" xframes (_fun _string -> _void)))
+
+(define setChildren
+  (get-ffi-obj "setChildren" xframes (_fun _int _string -> _void)))
+
+(define font-defs (file->string "./font-defs.json"))
+(define theme (file->string "./theme.json"))
+
+(define (onInit) (displayln "received!\n"))
+
+(define onTextValueChanged
+  (lambda (id value)
+    (displayln "Initialization is running...")))
+
+(define onComboValueChanged
+  (lambda (id selected-index)
+    (displayln "Initialization is running...")))
+
+(define onNumericValueChanged
+  (lambda (id value)
+    (displayln "Initialization is running...")))
+
+(define onBooleanValueChanged
+  (lambda (id value)
+    (displayln "Initialization is running...")))
+
+(define onMultipleNumericValuesChanged
+  (lambda (id values-pointer num-values)
+    (displayln "Initialization is running...")))
+
+(define onClick
+  (lambda (id)
+    (displayln "Initialization is running...")))
+
+(init 
+    "./assets"
+    font-defs
+    theme
+    onInit
+    onTextValueChanged 
+    onComboValueChanged 
+    onNumericValueChanged 
+    onBooleanValueChanged 
+    onMultipleNumericValuesChanged 
+    onClick
+)
+
+(define (infinite-loop)
+  (define current (current-thread))
+  (printf "Current thread: ~a\n" current)
+  (let loop ()
+    (sleep 0.1)
+    (loop)))
+
+(infinite-loop)
--- test.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(require ffi/unsafe)
-
-;; Load the shared C library
-(define lib (ffi-lib "./xframesshared.dll")) ;; Adjust path as necessary
-
-(display "Hello, World!")

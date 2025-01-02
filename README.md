bcc68d658fdb056fd2330a4eb61da0c2d1ad4b61
Andrea Mancuso <and.mancuso@googlemail.com>
2025-01-02 21:51:47 +0100

    Update README.md, font-defs.json, and main.rkt

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # xframes-racket
+
+## Instructions
+
+### Install Racket
+
+#### Windows
+
+I recommend using winget:
+
+`winget install racket.racket`
+
+### Install dependencies
+
+- `raco pkg install json`
+- `raco pkg install json-sourcery-lib`
+
+## Running the application
+
+`racket main.rkt`
--- main.rkt
+++ main.rkt
@@ -10,6 +10,7 @@
          ffi/unsafe/os-thread
          racket/match
          racket/runtime-path
+         rebellion/type/enum
          json)
 
 
@@ -47,10 +48,122 @@
 (define setChildren
   (get-ffi-obj "setChildren" xframes (_fun _int _string -> _void)))
 
-(define font-defs (file->string "./font-defs.json"))
+; (define font-defs (file->string "./font-defs.json"))
 (define theme (file->string "./theme.json"))
 
-(define (onInit) (displayln "received!\n"))
+; export const theme2Colors = {
+;   darkestGrey: "#141f2c",
+;   darkerGrey: "#2a2e39",
+;   darkGrey: "#363b4a",
+;   lightGrey: "#5a5a5a",
+;   lighterGrey: "#7A818C",
+;   evenLighterGrey: "#8491a3",
+;   black: "#0A0B0D",
+;   green: "#75f986",
+;   red: "#ff0062",
+;   white: "#fff",
+; };
+
+(define theme-colors 
+    (hasheq 
+        'darkestGrey "#141f2c" 
+        'darkerGrey "#2a2e39" 
+        'darkGrey "#363b4a" 
+        'lightGrey "#5a5a5a" 
+        'lighterGrey: "#7A818C" 
+        'evenLighterGrey "#8491a3" 
+        'black "#0A0B0D"  
+        'green: "#75f986" 
+        'red "#ff0062"
+        'white "#fff" 
+    )
+)
+
+
+(define ImGuiCol
+  '((Text . 0)
+    (TextDisabled . 1)
+    (WindowBg . 2)
+    (ChildBg . 3)
+    (PopupBg . 4)
+    (Border . 5)
+    (BorderShadow . 6)
+    (FrameBg . 7)
+    (FrameBgHovered . 8)
+    (FrameBgActive . 9)
+    (TitleBg . 10)
+    (TitleBgActive . 11)
+    (TitleBgCollapsed . 12)
+    (MenuBarBg . 13)
+    (ScrollbarBg . 14)
+    (ScrollbarGrab . 15)
+    (ScrollbarGrabHovered . 16)
+    (ScrollbarGrabActive . 17)
+    (CheckMark . 18)
+    (SliderGrab . 19)
+    (SliderGrabActive . 20)
+    (Button . 21)
+    (ButtonHovered . 22)
+    (ButtonActive . 23)
+    (Header . 24)
+    (HeaderHovered . 25)
+    (HeaderActive . 26)
+    (Separator . 27)
+    (SeparatorHovered . 28)
+    (SeparatorActive . 29)
+    (ResizeGrip . 30)
+    (ResizeGripHovered . 31)
+    (ResizeGripActive . 32)
+    (Tab . 33)
+    (TabHovered . 34)
+    (TabActive . 35)
+    (TabUnfocused . 36)
+    (TabUnfocusedActive . 37)
+    (PlotLines . 38)
+    (PlotLinesHovered . 39)
+    (PlotHistogram . 40)
+    (PlotHistogramHovered . 41)
+    (TableHeaderBg . 42)
+    (TableBorderStrong . 43)
+    (TableBorderLight . 44)
+    (TableRowBg . 45)
+    (TableRowBgAlt . 46)
+    (TextSelectedBg . 47)
+    (DragDropTarget . 48)
+    (NavHighlight . 49)
+    (NavWindowingHighlight . 50)
+    (NavWindowingDimBg . 51)
+    (ModalWindowDimBg . 52)
+    (COUNT . 53)))
+
+
+(define (enum-value key)
+  (cdr (assoc key ImGuiCol)))
+
+(define theme-hash
+  (hasheq 'colors
+    (hasheq
+      (string->symbol (number->string (enum-value 'Text))) (list (hash-ref theme-colors 'white) 1)
+    )
+  )
+)
+
+(define font-defs-hash (hasheq 'defs (hasheq 'name "roboto-regular" 'size "16")))
+(define font-defs
+    (jsexpr->string	font-defs-hash)
+)
+
+(define theme-json
+    (jsexpr->string	theme-hash)
+)
+
+(displayln theme-json)
+
+(define (onInit)
+    (thunk
+        (displayln "init")
+    )
+)
 
 (define onTextValueChanged
   (lambda (id value)
@@ -91,7 +204,8 @@
 
 (define (infinite-loop)
   (define current (current-thread))
-  (printf "Current thread: ~a\n" current)
+;   (printf "Current thread: ~a\n" current)
+  (displayln "Press CTRL+C to terminate")
   (let loop ()
     (sleep 0.1)
     (loop)))
